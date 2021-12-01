49985bdf77f8eadc4cfe7e87774c2d7f38e28d16
blakehagen <blakehagen@gmail.com>
2021-11-30 19:21:20 -0700

    mint and burn erc721 for hw6

--- homework6/VolcanoToken.sol
+++ homework6/VolcanoToken.sol
@@ -3,14 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract VolcanoToken is Ownable, ERC721 {
-    constructor() ERC721("VolcanoToken", "VLT") {
-        _safeMint(msg.sender, 2);
-    }
+contract VolcanoToken is Ownable, ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    uint256 tokenId;
+    constructor() ERC721("VolcanoToken", "VLT") {}
 
     struct TokenMetadata {
         uint256 tokenId;
@@ -18,6 +18,42 @@ contract VolcanoToken is Ownable, ERC721 {
         string tokenURI;
     }
 
-    mapping(address => TokenMetadata[]) public tokens; // TODO => how to create an array of structs as the value of a mapping
+    mapping(address => TokenMetadata[]) public tokens;
+
+    function getTokensFromAddress(address _user) public view returns (TokenMetadata[] memory) {
+        return tokens[_user];
+    }
+
+    function mintToken() public {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        string memory nftString = "some cool nft string";
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, nftString);
+        addMintedToken(msg.sender, newItemId, block.timestamp, nftString);
+    }
+
+    function burnToken(uint256 _tokenId) public {
+        // make sure only token owner can burn!
+        for (uint i=0; i < tokens[msg.sender].length; i++) {
+            if (tokens[msg.sender][i].tokenId == _tokenId) {
+                _burn(_tokenId);
+                delete tokens[msg.sender][i];
+            }
+        }
+    }
+
+    function addMintedToken(address _owner, uint256 _tokenId, uint _timestamp, string memory _tokenURI) internal {
+        tokens[_owner].push(TokenMetadata(_tokenId, _timestamp, _tokenURI));
+    }
+
+    // function handleBurn(uint256 _tokenId, address _owner) internal {
+    //     // remove from mapping
+    //      for (uint i=0; i < tokens[_owner].length; i++) {
+    //         if (tokens[_owner][i].tokenId == _tokenId) {
+    //             delete tokens[_owner][i];
+    //         }
+    //     }
+    // }
 
 }
